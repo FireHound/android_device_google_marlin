8a2583ae0096e2bddd61eb8d1118cd40403ef50a
razorloves <razorloves@gmail.com>
2018-08-10 02:41:27 -0500

    marlin: Override theme overlay property
    This prevents the google pixel theme overlay on /vendor
    from breaking the lineage styles and accents theming.
    
    Change-Id: I96223c21f527131156f945a8c39f99e13e53a22f

--- device-lineage.mk
+++ device-lineage.mk
@@ -11,6 +11,10 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-lineage
 
+# Theme
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.boot.vendor.overlay.theme=org.lineageos.overlay.dark
+
 # Pixel Experience
 PRODUCT_COPY_FILES += \
     device/google/marlin/nexus.xml:system/etc/sysconfig/nexus.xml
